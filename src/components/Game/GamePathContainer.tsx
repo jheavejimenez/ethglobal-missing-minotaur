35e02f543770937c7f427e86df1d85d3c54effe3
Jack <jackdagusen13@gmail.com>
2022-03-08 02:31:27 +0800

    Add Moralis

--- src/components/Game/GamePathContainer.tsx
+++ src/components/Game/GamePathContainer.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import styled, { css, keyframes } from "styled-components";
 import { NetworkBtn } from '../Button';
 import TileComponent from './Tile/TileComponent';
+import { chainId, chainName, currencyName, currencySymbol, rpcUrl, blockExplorerUrl} from '../../constants/moralisConstants'
+import { useMetaMask } from "metamask-react";
+import { useMoralis } from "react-moralis";
+import Web3 from 'web3';
+
 
 const PathContainer = styled.div`
     display: flex;
@@ -18,20 +23,82 @@ const TileContainer = styled.div`
     border: 1px solid white;
 `;
 
+
 function GamePathContainer() {
+    const { authenticate, isAuthenticated, isAuthenticating, logout, Moralis } = useMoralis();
+
+    const SwitchNetwork = async () => {
+        const test = await Moralis.enableWeb3();
+
+        if (!test) {
+            try {
+                await Moralis.switchNetwork(chainId);
+            } catch (error: any) {
+                if (error.code === '4902') {
+                    try {
+                        await test.provider.request({
+                            method: "wallet_addEthereumChain",
+                            params: [
+                            {
+                                chainId: "0x13881",
+                                chainName: "Mumbai",
+                                rpcUrls: ["https://rpc-mumbai.matic.today"],
+                                nativeCurrency: {
+                                    name: "Matic",
+                                    symbol: "Matic",
+                                    decimals: 18,
+                                },
+                                blockExplorerUrls: ["https://explorer-mumbai.maticvigil.com"],
+                                },
+                            ],
+                        });
+                        await Moralis.switchNetwork(chainId);
+                    } catch (error: any) {
+                        console.log(error);
+                    }
+                }
+            }
+        } else {
+            alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html');
+        }
+    }
+
+    const login = async () => {
+        // const test = await Moralis.enableWeb3();
+        // const ethers = Moralis.web3Library
+        // const a = test.provider.request
+        // console.log(a);
+        if (!isAuthenticated) {
+            await authenticate({signingMessage: "Log in using Moralis"})
+            .then(function (user) {})
+            .catch(function (error) 
+            {console.log(error);});
+        }
+    }
+
+    const logOut = async () => {
+        await logout();
+        console.log("logged out");
+    }
 
     return (
         <PathContainer>
             <NetworkBtn
-                onClick={() => {
-                    alert("sample");
-                }}
+                onClick={login}
                 title={"Connect Wallet"}
             >
                 Connect Wallet
             </NetworkBtn>
+        <button 
+            onClick={logOut} 
+            disabled={isAuthenticating}
+            >
+                Logout
+        </button>
+
         </PathContainer>
     );
 }
+
 
 export default GamePathContainer;
---
+++ src/constants/moralisConstants.ts
@@ -0,0 +1,9 @@
+export const MORALIS_APP_ID = "XuzW3wiF0AqpWWEgPb0RIuzsID3Cy7TkK6LACwe7";
+export const MORALIS_SERVER_URL = "https://sxuwakb1qvar.usemoralis.com:2053/server";
+
+export const chainId =  0x13881;
+export const chainName = "Mumbai Testnet";
+export const currencyName = "MATIC";
+export const currencySymbol = "MATIC";
+export const rpcUrl = "https://rpc-mumbai.maticvigil.com/";
+export const blockExplorerUrl = "https://polygonscan.com/";
