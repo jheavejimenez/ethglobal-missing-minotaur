7e45c4e6f6915f4ab8c219cf43d6b7c131448731
Jheave <jimenezjheave123@gmail.com>
2022-03-08 23:56:37 +0800

    add withdraw script

--- contracts/MissingMinotaur.sol
+++ contracts/MissingMinotaur.sol
@@ -1,31 +1,80 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
  
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 
-contract MissingMinotaur is ERC721URIStorage {
+contract MissingMinotaur is ERC721, Ownable {
+  using Strings for uint256;
   using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+  Counters.Counter private supply;
 
+  string public baseURI = "";
+  string public baseExtension = ".json";
+
+  uint256 public constant MAX_SUPPLY = 999;
+  uint256 public mintPrice = 0.01 ether;
   
-  constructor() ERC721 ("Missing Minotaur", "Missing Minotaur") {
-    console.log("This is my NFT contract. Woah!");
+  constructor() ERC721 ("Missing Minotaur", "Missing Minotaur") {}
+
+  modifier mintCondition() {
+    require(supply.current() <= MAX_SUPPLY, "Max supply exceeded!");
+    require(supply.current() < 899, "invalid claim");
+    _;
   }
 
-  function mintNFT() public {
-    uint256 newItemId = _tokenIds.current();
+  function totalSupply() public view returns (uint256) {
+    return supply.current();
+  }
 
-    _safeMint(msg.sender, newItemId);
+  function mintNFT() public payable mintCondition {
+    require(msg.value >= mintPrice, "Insufficient funds!");
+    _safeMint(msg.sender, supply.current());
+    supply.increment();
 
-    // Set the NFTs data.
-    _setTokenURI(newItemId, "INSERT_JSON_URL_HERE");
-    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+  }
 
-    // increment the tokenIds counter
-    _tokenIds.increment();
+  function ownerClaim(address _receiver) public onlyOwner {
+    require(supply.current() > 899 && supply.current() < MAX_SUPPLY, "invalid claim");
+    _safeMint(_receiver, supply.current());
+    supply.increment();
+  }
 
+  function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+    uint256 currentTokenId = 1;
+    uint256 ownedTokenIndex = 0;
+    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= MAX_SUPPLY) {
+      address currentTokenOwner = ownerOf(currentTokenId);
+
+      if (currentTokenOwner == _owner) {
+          ownedTokenIds[ownedTokenIndex] = currentTokenId;
+          ownedTokenIndex++;
+      }
+      currentTokenId++;
+    }
+    return ownedTokenIds;
+  }
+    
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    require(_exists(tokenId),"ERC721Metadata: URI query for nonexistent token");
+    return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), baseExtension)) : "";
+  }
+
+  function withdraw() public onlyOwner {
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
   }
 }
--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -5,9 +5,18 @@ const main = async () => {
   console.log("Contract deployed to:", nftContract.address);
 
   // Call the function.
-  let txn = await nftContract.mintNFT()
+  let txn = await nftContract.mintNFT({value: hre.ethers.utils.parseEther('10')});
   // Wait for it to be mined.
   await txn.wait()
+ 
+  txn = await nftContract.mintNFT({value: hre.ethers.utils.parseEther('10')});
+  await txn.wait()
+
+  console.log("Minted NFT");
+
+  // How much money is in here?
+  const balance = await hre.ethers.provider.getBalance(nftContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
 };
 
