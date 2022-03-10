8081c73d98c3d5debaa6b23fa8f65cea3cd09c46
Jheave <jimenezjheave123@gmail.com>
2022-03-11 03:34:17 +0800

    add deploy scripts and config deplay env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+ module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: secret.url,
+      accounts: [secret.key],
+    }
+  },
+  etherscan: {
+    apiKey: secret.etherscanAPIKey,
+  }
 };
