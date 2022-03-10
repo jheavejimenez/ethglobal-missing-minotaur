803f306c265f29df3d5e137e06ec14c3ead2a5e1
Jheave <jimenezjheave123@gmail.com>
2022-03-11 02:51:54 +0800

    add and refractor contracts

--- contracts/MissingMinotaurMaze.sol
+++ contracts/MissingMinotaurMaze.sol
@@ -1,17 +1,117 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract MissingMinotaurMaze is ERC721, ReentrancyGuard, Ownable{
+contract MissingMinotaurMaze is ERC721, ReentrancyGuard, Ownable {
     using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    struct Puzzle {
+        uint256 seed;
+        uint256[] clicks;
+        uint256 puzzleLength;
+    }
 
-    constructor() ERC721("MissingMinotaurMaze", "MMM") {}
-    // create a function that generate a pattern in x,y coordinates
+    struct Game {
+        Puzzle puzzle;
+        address nftContractAddress;
+        uint256 nftId;
+    }
 
+    Counters.Counter private supply;
+
+    uint256 public mintPrice = 1 ether;
+
+    uint256 public randomNumber = 50;
+
+    constructor() ERC721('Missing Minotaur Maze', 'MMM') {}
+
+    function _collatzNext(uint256 x) private pure returns (uint256 result) {
+        if (x % 2 == 0) {
+            return x / 2;
+        }
+        return 3 * x + 1;
+    }
+
+    function _min(uint256 a, uint256 b) private pure returns (uint256) {
+        if (a > b) return b;
+        return a;
+    }
+
+    function _max(uint256 a, uint256 b) private pure returns (uint256) {
+        if (a > b) return a;
+        return b;
+    }
+
+    function _subtractNoNegative(uint256 a, uint256 b)
+        private
+        pure
+        returns (uint256)
+    {
+        if (a > b) return a - b;
+        return 0;
+    }
+
+
+    function _generatePuzzle(uint256 seed)
+        private
+        pure
+        returns (Puzzle memory)
+    {
+        uint256[] memory clicks = new uint256[](10);
+
+        uint256 collatzCurrent = seed;
+        uint256 puzzleLength;
+
+        for (
+            puzzleLength = 0;
+            puzzleLength < clicks.length && collatzCurrent > 1;
+            puzzleLength++
+        ) {
+            clicks[puzzleLength] = collatzCurrent % 36;
+            collatzCurrent = _collatzNext(collatzCurrent);
+        }
+
+        return Puzzle(seed, clicks, puzzleLength);
+    }
+
+    function puzzle(uint256 tokenId) public view returns (Puzzle memory) {
+        require(_exists(tokenId), 'MMM: Puzzle does not exist.');
+        return _generatePuzzle(tokenId + randomNumber);
+    }
+
+    function mint() public payable nonReentrant {
+        require(
+            msg.value >= mintPrice,
+            'MMM: Amount of MATIC sent is incorrect.'
+        );
+        // TODO: require that player has Missing Minotaur NFT
+
+        supply.increment();
+        _safeMint(msg.sender, supply.current());
+    }
+
+    function burnAndClaimReward(uint256 tokenId, uint256[] memory clicks)
+        public
+        virtual
+    {
+        require(_exists(tokenId), 'MMM: Puzzle does not exist.');
+        require(
+            ownerOf(tokenId) == msg.sender,
+            'MMM: burning from incorrect owner'
+        );
+        // TODO: require that not more than 20 minutes has passed
+        require(clicks.length != 0, 'MMM: Solution required.');
+        // TODO: require that clicks do solve the puzzle
+        _burn(tokenId);
+        payable(msg.sender).transfer(1 ether);
+    }
+
+    function withdraw() public onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
 }
