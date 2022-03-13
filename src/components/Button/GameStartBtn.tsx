6ca97b1ed10b53fd9384ebce97bb2be5391e8d22
JRParreno <jhonrhayparreno22@gmail.com>
2022-03-13 14:46:33 +0800

    fix start demo url

--- src/components/Button/GameStartBtn.tsx
+++ src/components/Button/GameStartBtn.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import styled, { CSSProperties } from "styled-components";
 
 const GameStartBtn = styled.a`
-background-color: #000; /* Green */
-border: none;
-color: white;
-padding: 15px 32px;
-text-align: center;
-text-decoration: none;
-display: inline-block;
-font-size: 16px;
-border-radius: 30px;
+    background-color: #000;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 30px;
 `;
 
 export default function NavLink(props: { name: string, urlName: string, style?: CSSProperties }) {
